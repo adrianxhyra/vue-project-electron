d82fdc0645e14427350629c1ca053181b07a02d1
Adrian Xhyra <adrianxhyra1995@gmail.com>
2022-07-06 08:59:55 +0200

    fdrgfg

--- vue.config.js
+++ vue.config.js
@@ -1,8 +1,18 @@
 module.exports = {
+  configureWebpack: {
+    devtool: 'source-map'
+  },
   pluginOptions: {
     electronBuilder: {
       builderOptions: {
-        publish: ['github']
+        publish: [
+          {
+            provider: 'github',
+            owner: 'adrianxhyra',
+            private: false,
+          }
+        ]
+        // publish: ['github']
       },
       preload: "src/preload.js", // this one
     }
